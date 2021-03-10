ebdff4a5fb0da03137f5459dcc8ef9bec5a11591
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2021-03-10 13:32:39 +1300

    hue test is done

--- Assets/Scripts/HueControl.cs
+++ Assets/Scripts/HueControl.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class HueControl : MonoBehaviour
 {
+    Color newColor;
     // Start is called before the first frame update
     void Start()
     {
+        //get the objects material renderer
         var rCubeRenderer = GetComponent<Renderer>();
+        //Make a Colour
+        newColor = Color.gray;
+        //Set the Colour
+        rCubeRenderer.material.SetColor("_Color", newColor);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var rCubeRenderer = GetComponent<Renderer>();
+        newColor = new Color(newColor.r + 0.1f * Time.deltaTime, newColor.b + 0.1f * Time.deltaTime, newColor.g + 0.1f * Time.deltaTime, newColor.a);
+        rCubeRenderer.material.SetColor("_Color", newColor);
     }
 }
