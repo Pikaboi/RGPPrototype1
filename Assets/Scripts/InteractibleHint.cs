91f56010df6aaae1ae7c358da90b258020b69b03
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2021-03-11 11:47:20 +1300

    Interaction range detection

---
+++ Assets/Scripts/InteractibleHint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractibleHint : MonoBehaviour
+{
+    public Image buttonPrompt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonPrompt.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            buttonPrompt.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            buttonPrompt.enabled = false;
+        }
+    }
+}
