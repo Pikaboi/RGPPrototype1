baee312729b1a120c1c380579a67fb709ec0892e
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2021-03-10 15:05:40 +1300

    transition scenes

---
+++ Assets/Scripts/PanelControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelControl : MonoBehaviour
+{
+    private Image iPanel;
+    private bool bFade = false;
+    private bool bSolidify = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        iPanel = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bFade == true)
+        {
+            fading();
+        }
+
+        if (bSolidify == true)
+        {
+            solidifying();
+        }
+    }
+
+    //Player calls object to turn item transparent
+    public void startFade()
+    {
+        bFade = true;
+        bSolidify = false;
+        Debug.Log("Fade");
+    }
+
+    //Player calls this object to turn item solid
+    public void startSolidify()
+    {
+        bSolidify = true;
+        bFade = false;
+    }
+
+    private void fading()
+    {
+        iPanel.Color = new Color(iPanel.Color.r, iPanel.Color.b, iPanel.g, iPanel.Color.a + 0.1 * Time.deltaTime);
+    }
+
+    private void solidifying()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    //Get the scenes panel
+    [SerializeField]
+    private PanelControl goPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Check for a collision on a trigger
+    void OnTriggerEnter(Collider other)
+    {
+        //Check which warp it is.
+        if(other.tag == "warppoint")
+        {
+            goPanel.startFade();
+        }
+    }
+}
