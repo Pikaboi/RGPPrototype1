7aab8be06b7928f81407144ab8c6b13a2de995fb
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2021-03-10 13:01:28 +1300

    line

--- Assets/Scripts/HueControl.cs
+++ Assets/Scripts/HueControl.cs
@@ -7,7 +7,7 @@ public class HueControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var rCubeRenderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
