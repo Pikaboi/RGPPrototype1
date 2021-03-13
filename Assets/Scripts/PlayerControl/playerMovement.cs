355c18adfdcb124f425a5aa864f683d46697f360
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2021-03-13 13:19:26 +1300

    metas

--- Assets/Scripts/PlayerControl/playerMovement.cs
+++ Assets/Scripts/PlayerControl/playerMovement.cs
@@ -93,6 +93,7 @@ public class playerMovement : MonoBehaviour
         {
             //Scale back up after crouch
             transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 2, transform.localScale.z);
+            transform.position = new Vector3(transform.position + 1.0f);
         }
     }
 
