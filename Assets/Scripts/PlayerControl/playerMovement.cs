a5d8318d90ddc9b2ac4533d8ca894ed7f3036ef8
himchawla <himchawla43@gmail.com>
2021-03-15 20:41:15 +1300

    Revert "Merge branch 'master' into thunderChawla"
    This reverts commit 713daef1ffeafcff162a2331783a693c1b1a4ade, reversing
    changes made to 153c2b84224994e7efa6c85f7b0b8d4aed10c9b2.

--- Assets/Scripts/PlayerControl/playerMovement.cs
+++ Assets/Scripts/PlayerControl/playerMovement.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class playerMovement : MonoBehaviour
 {
@@ -105,11 +104,6 @@ public class playerMovement : MonoBehaviour
             //Scale back up after crouch
             //transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 2, transform.localScale.z);
             GetComponent<CharacterController>().height = 3.2f;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Application.Quit();
         }
     }
 
