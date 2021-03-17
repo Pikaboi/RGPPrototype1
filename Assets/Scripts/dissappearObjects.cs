39c6db864d3d8c91bb87e152ac18485b8be596c7
himchawla <58020143+himchawla@users.noreply.github.com>
2021-03-17 15:56:08 +1300

    Dissapear fixing

--- Assets/Scripts/dissappearObjects.cs
+++ Assets/Scripts/dissappearObjects.cs
@@ -18,17 +18,47 @@ public class dissappearObjects : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Passes > dissappeared)
+        
+    }
+
+    void OnDisable()
+    {
+        if (Passes % 2 == 1)
         {
-            foreach (GameObject i in GameObject.FindGameObjectsWithTag("Dissappearing"))
+            foreach (GameObject i in GameObject.FindGameObjectsWithTag("Teenage"))
             {
-                if (!i.activeSelf)
-                    continue;
-                else
+
                 {
-                    dissappeared++;
-                    i.SetActive(false);
-                    break;
+                    i.GetComponent<MeshRenderer>().enabled = (false);
+                }
+            }
+
+            foreach (GameObject i in GameObject.FindGameObjectsWithTag("Child"))
+            {
+
+                {
+                    i.GetComponent<MeshRenderer>().enabled = (true);
+                }
+            }
+        }
+
+        if (Passes % 2 == 0)
+        {
+            foreach (GameObject i in GameObject.FindGameObjectsWithTag("Teenage"))
+            {
+
+                {
+                    i.GetComponent<MeshRenderer>().enabled = true;
+                }
+            }
+
+            foreach (GameObject i in GameObject.FindGameObjectsWithTag("Child"))
+            {
+
+
+
+                {
+                    i.GetComponent<MeshRenderer>().enabled = (false);
                 }
             }
         }
