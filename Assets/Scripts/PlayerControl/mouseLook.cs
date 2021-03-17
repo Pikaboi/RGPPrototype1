992f0c531288a6d3eb88bd8ae984fce0c929ef52
Braden Cleur <braden.cleur@gmail.com>
2021-03-17 16:14:34 +1300

    changed some stuff for executable

--- Assets/Scripts/PlayerControl/mouseLook.cs
+++ Assets/Scripts/PlayerControl/mouseLook.cs
@@ -14,6 +14,7 @@ public class mouseLook : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
