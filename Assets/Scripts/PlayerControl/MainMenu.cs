e15f2644a4813ea2df2c6b64fe4fdb361d70df42
Braden Cleur <braden.cleur@gmail.com>
2021-03-17 15:50:09 +1300

    MenuLoops

--- Assets/Scripts/PlayerControl/MainMenu.cs
+++ Assets/Scripts/PlayerControl/MainMenu.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.None;
     }
 
     // Update is called once per frame
@@ -24,5 +25,10 @@ public class MainMenu : MonoBehaviour
     public void quitGame()
     {
         Application.Quit();
+    }
+
+    public void backToMain()
+    {
+        SceneManager.LoadScene(0);
     }
 }
